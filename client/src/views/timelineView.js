2b750573ad9e36e18715c0c8394a0f1499be617e
Ross Crichton <rcrichton87@googlemail.com>
2017-04-21 16:15:18 +0100

    did some css styling, added a background image

--- client/src/views/timelineView.js
+++ client/src/views/timelineView.js
@@ -7,12 +7,10 @@ TimelineView.prototype = {
   render: function(explorers, mapView){
     explorers.forEach(function(explorer){
       var li = document.createElement('li')
-      // li.className = 'timeline-item'
       var div = document.createElement('div')
       div.innerText = explorer.date + '\n' + explorer.title
       li.appendChild(div)
       this.timelineList.appendChild(li)
-
       li.addEventListener('click', function(){
         mapView.clearMarkers()
         mapView.addMarkers(explorer)
