865b847af60ee25e81858624098501b9c493ffda
Alice Loudon <alice_loudon@icloud.com>
2017-04-21 10:10:47 +0100

    Add explorers to seeds.js

---
+++ db/seeds.js
@@ -0,0 +1,71 @@
+use explorers
+
+db.discoveries.insert([
+  {
+    date: 1492, 
+    title: "Columbus Discovers America", 
+    name: "Christopher Columbus",
+    nationality: "Italian",
+    startcoord: {lat: 41.680588, lng: -87.835328},
+    endcoord: {lat: 13.692940, lng: -89.218191}, 
+    info: "After stopping in the Canary Islands off the coast of Africa, Columbus’ ships hit the open seas. Covering about 150 miles a day, the trip was long and arduous. The crew was afraid of sea monsters and grew more restless every day that land was not sighted. Columbus offered a reward for the first person to sight land. On October 12, a crew member aboard the Pinta sighted one of the Bahama Islands. Columbus set foot on what he believed was one of the Spice Islands, a group of islands in Asia (now known as Indonesia), where valuable spices and riches came from. He named the land San Salvador. Columbus failed to find the riches he expected, and continued to search for China. He next visited Cuba and Hispaniola (Dominican Republic). He encountered native peoples who he named “Indians” because he believed they were inhabitants of the Indies.", 
+    image: "http://assets0.ordienetworks.com/misc/Screen%20Shot%202013-10-11%20at%209.22.15%20AM.png" 
+  },
+  {
+    date: 1914, 
+    title: "Almost made it to the South Pole", 
+    name: "Sir Ernest Shackleton",
+    nationality: "Anglo-Irish"
+    startcoord: {lat: , lng: -},
+    endcoord: {lat: , lng: -}, 
+    info: "Shackleton was an Antarctic explorer, best known for leading the 'Endurance' expedition of 1914-16. In 1914, Shackleton made his third trip to the Antarctic with the ship 'Endurance', planning to cross Antarctica via the South Pole. Early in 1915, 'Endurance' became trapped in the ice, and ten months later sank. Shackleton's crew had already abandoned the ship to live on the floating ice. In April 1916, they set off in three small boats, eventually reaching Elephant Island. Taking five crew members, Shackleton went to find help. In a small boat, the six men spent 16 days crossing 1,300 km of ocean to reach South Georgia and then trekked across the island to a whaling station. The remaining men from the 'Endurance' were rescued in August 1916. Not one member of the expedition died.", 
+    image: "https://www.biography.com/.image/t_share/MTE1ODA0OTcxNzgzMDYzMDUz/sir-ernest-henry-shackleton-9480091-1-raw.jpg" 
+  },
+  {
+    date: 1911, 
+    title: "The first expedition to reach the South Pole", 
+    name: "Roald Amundsen",
+    nationality: "Norwegian"
+    startcoord: {lat: 59.9139, lng: 10.7522},
+    endcoord: {lat: -90, lng: 0}, 
+    info: "Roald Amundsen was the first man to lead a successful expedition to the South Pole, famously arriving about a month before Scott and his party that set out at around the same time. Amundsen used dog sleds, his party was well organised and well prepared with the primary intention of reaching the pole, rather than other exploration or scientific discovery. He was the first person to visit both the North and the South poles.",
+    image: "http://photos.state.gov/libraries/newzealand/8558/ice2012/Roald-Amundsen_1767131b.jpg" 
+  },
+  {
+    date: "June 1st 1937",
+    title: "Flying across the world",
+    name: "Amelia Earhart",
+    startcoord: {lat: -6.314993, lng: 143.955550},
+    endcoord: {lat: -4.677919, lng: -174.519597},
+    info: "On June 1st, 1937, Amelia Earhart and co-pilot Fred Noonan again set off to fly around the world. They first flew to San Juan, Puerto Rico, and then eastward toward Africa. Amelia and Fred flew to the Red Sea, to Karachi, Pakistan and to Calcutta, India. Amelia and Fred continued to Rangoon, Bangkok and Singapore. At Port Darwin, Australia, Amelia and Fred were able to make crucial repairs to their airplane. Amelia reached Lae, New Guinea on June 29th. They had flown 22,000 miles and had 7,000 left to go. Amelia had become an international hero, and the world was captivated by her determination. At this point, however, Amelia was said to be exhausted and may have alluded to the possibility that her co-pilot, Fred Noonan, was drinking. However, trouble lurked for Amelia and Fred. Shortly after Amelia left New Guinea on July 2, she sent several distress calls. While her distress calls were received, Amelia could not hear the return messages because she could not find the correct frequency on her radio. Amelia and Fred were off-course and lost over the vast Pacific Ocean. They did not have enough gas to reach their target – The Howland Islands. Amelia and Fred would never be seen or heard from again. President Roosevelt sent nine naval ships and 66 aircraft to search the area. On July 18 the search was called off.",
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/d2/Amelia_Earhart_1935.jpg"
+  },
+  {
+    date: September 20th 1519,
+      title: "Sailing around the world",
+      name: "Ferdinand Magellan",
+      startcoord: {lat: -6.314993, lng: 143.955550},
+      endcoord: {lat: -4.677919, lng: -174.519597},
+      info: "On September 20, 1519 Magellan and 237 crew members set sail on five ships from Sanlucar de Barrameda in the hopes of finding a shortcut to the Spice Islands. After three long months of sailing the Atlantic, Magellan and his crew anchored near Rio de Janeiro in the present day South American nation of Brazil. After trading with local natives, Magellan and his men quickly set sail again, ever worried about the threat of Portuguese ships. As the expedition continued, the weather got worse and several crew members were executed for trying to take over the ship. Others were starving or suffering from frostbite. As the ships neared the southern tip of South America, one smashed into the beach and lost all supplies. Nevertheless, in October of 1520, Magellan and his crew crossed the treacherous straits at the tip of South America, which became known as the Straits of Magellan. /n As the expedition passed through the straits, they entered the vast Pacific Ocean. Things got worse before they got better. The crew suffered from extreme hunger and were forced to survive by eating rats, sawdust, leather and even maggots. At least 20 men died from disease, particularly Scurvy (caused by a lack of Vitamin C) and starvation. On March 6, the ships finally reached land – the Pacific island of Guam. Nevertheless, the crew members were able to collect clean water and food, despite being on the alert for attacks by the natives. Soon, the crew sailed to the Philippine Islands, where they converted natives to Christianity. During an attempt to convert the native chief, Mactan, to Christianity, Magellan was wounded by an arrow that was driven through his foot by a native warrior. Mactan, who had become upset with Magellan’s insistence that he convert, ordered his warriors to attack. They killed Magellan on April 27, 1521. Most of Magellan’s crew escaped and set sail./n With the loss of their leader, Juan Sebastian Del Cano took control of the ship and sailed for Spain. Only two ships and 47 men remained. Portuguese forces captured one of the ships, leaving the Victoria as the only ship left. Severe storms and Portuguese attacks battered the Victoria. As Portuguese forces bore down on the expedition near the Cape Verde Islands, Del Cano was forced to continue toward Spain without supplies or rest. Finally, on September 8, 1522, The Victoria made it back to Spain with only 18 men surviving, hence completing the first circumnavigation of the world.",
+      image: "https://en.wikipedia.org/wiki/Ferdinand_Magellan#/media/File:Ferdinand_Magellan.jpg"
+  },
+  {
+    date: 1953, 
+    title: "First Ascent of Mount Everest", 
+    name: "Edmund Hillary",
+    nationality: "New Zealand",
+    startcoord: {lat: 41.680588, lng: -87.835328},
+    endcoord: {lat: 13.692940, lng: -89.218191}, 
+    info: "Famous For: Being the first to reach the summit of Mount Everest. New Zealander Edmund Hillary and fellow mountaineer Tenzing Norgay were the first known and confirmed climbers ever to make it to the peak of Mount Everest in 1953. Other notable feats that Hillary accomplished in addition to his Mount Everest adventure is that he also reached both the North and South Pole, which made him the first man to ever reach all three landmarks. During World War II, Hillary was part of the New Zealand Air Force working as a navigator.", 
+    image: "" 
+  },
+  {
+     date: 800,
+     title: "Maori colonisation of New Zealand",
+     name: "Kupe",
+     startcoord: {lat: -17.533333, lng: -149.833333},
+     endcoord: {lat: -36.848461, lng:  174.763336},
+     info: "Traditional Polynesian navigation was used for thousands of years to make long voyages across thousands of miles of the open Pacific Ocean. Navigators travelled to small inhabited islands using wayfinding techniques and knowledge passed by oral tradition.  Polynesians are believed to have spread eastward from the Samoan Islands into the Marquesas, the Society Islands, the Hawaiian Islands and Easter Island; and south to New Zealand, leading to many distinctive but related cultures.",
+     image: "https://s-media-cache-ak0.pinimg.com/originals/7a/58/48/7a5848809181cfafbffaa79a8e2d0c8f.jpg"
+   }
+])
