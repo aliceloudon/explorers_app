995712029605022f094c904aba796320419bc47f
Ross Crichton <rcrichton87@googlemail.com>
2017-04-21 12:06:20 +0100

    commit before pulling

--- client/src/app.js
+++ client/src/app.js
@@ -11,12 +11,14 @@ var app = function(){
 
   var explorersList = new ExplorersList("http://localhost:3000/api/explorers")
   explorersList.makeRequest(function(explorers){
+
     explorers.forEach(function(explorer){
       var name = document.createElement('li')
       name.innerText = explorer.name
       var ul = document.querySelector('ul')
       ul.appendChild(name)
     })
+    
   })
 }
 
