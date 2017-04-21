cc91039d82bbefb1f47755ab1f260cabb9ac7a49
Ross Crichton <rcrichton87@googlemail.com>
2017-04-21 11:43:13 +0100

    map appears on screen

--- client/src/app.js
+++ client/src/app.js
@@ -2,6 +2,13 @@ var ExplorersList = require("./models/explorersList")
 var MapView = require("./views/mapView")
 
 var app = function(){
+
+  var mapContainer = document.querySelector('#map')
+  var center = {lat:0, lng:0}
+  var zoom = 1
+
+  var mapView = new MapView(mapContainer, center, zoom)
+
   var explorersList = new ExplorersList("http://localhost:3000/api/explorers")
   explorersList.makeRequest(function(explorers){
     explorers.forEach(function(explorer){
