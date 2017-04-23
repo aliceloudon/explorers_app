65c17770ba6fb5beda9e79592d2e3ed9d9c34b89
Ross Crichton <rcrichton87@googlemail.com>
2017-04-23 13:44:31 +0100

    explorers router will go to an questions api

--- controllers/explorers_router.js
+++ controllers/explorers_router.js
@@ -4,9 +4,15 @@ var explorersRouter = express.Router()
 var ExplorersQuery = require("../db/explorers_query")
 var explorersQuery = new ExplorersQuery()
 
-explorersRouter.get("/", function(req,res){
-  explorersQuery.all(function(docs){
+explorersRouter.get("/explorers", function(req,res){
+  explorersQuery.allExplorers(function(docs){
     res.json(docs)
+  })
+})
+
+explorersRouter.get("/quiz", function(req,res){
+  explorersQuery.allQuestions(function(docs){
+    res.json({question: "who climbed everest?"})
   })
 })
 
