79b011dcb072520d829b2f71536468f605628216
Rob <robflettcc@gmail.com>
2017-04-24 13:16:53 +0100

    adding image folder

--- client/src/models/tileGame.js
+++ client/src/models/tileGame.js
@@ -1,19 +1,3 @@
-// var game_array = ['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H','I','I','J','J','K','K','L','L']
-// var game_values = []
-// var tiles_id = []
-// var flipped_tiles = []
-
-
-// function newGame(){
-//   flipped_tiles = 0
-//   game_array = _.shuffle(game_array)
-//   var output = ''
-//   forEach(game_array, function(game_array_value, index){
-//     output += '<div id="tile_' + index +'" onclick="memoryFlipTile(this,' + game_array_value + ')"></div>'
-//   })
-//   document.getElementById('memory-game').innerHTML = output
-// }
-
 
 
 module.exports = tileGame
