bb14b8d4a27c344c62b09b065464cc97a430edec
Alice Loudon <alice_loudon@icloud.com>
2017-04-25 15:08:23 +0100

    Add radio buttons to change canvas paint tool

--- client/src/views/canvasView.js
+++ client/src/views/canvasView.js
@@ -6,13 +6,24 @@ var CanvasView = function(canvas, context){
 CanvasView.prototype = {
 
   render: function(){
+    
     var self = this
     this.canvas.onclick = function(event){
       var coords = self.canvas.getBoundingClientRect()
       console.log(coords.left, coords.top)
       console.log(event.clientX, event.clientY)
-      self.drawRectangle( (event.clientX - coords.left), (event.clientY - coords.top) )
+      // self.drawRectangle( (event.clientX - coords.left), (event.clientY - coords.top) )
+      self.drawCircle((event.clientX - coords.left), (event.clientY - coords.top))
     }
+
+    // var tools = document.querySelectorAll('input')
+    // tools.forEach(function(tool){
+    //   tool.onclick = function{
+    //     currentTool = this.value
+    //     canvas.onclick = canvasClickHandler
+    //   }
+    // })
+
   },
 
   drawRectangle: function(x, y){
@@ -20,11 +31,14 @@ CanvasView.prototype = {
     this.context.fillRect(x, y, 10, 10)
   },
 
-  // drawCircle: function(x, y){
-  //   this.context.beginPath()
-  //   this.context.arc(x, y, 5, 0, Math.PI*2, true)
-  //   this.context.stroke()
-  // }
+  drawCircle: function(x, y){
+    this.context.beginPath()
+    this.context.arc(x, y, 10, 0, Math.PI*2)
+    this.context.fillStyle = 'green'
+    this.context.fill()
+    this.context.strokeStyle = 'green'
+    this.context.stroke()
+  }
 
 }
 
