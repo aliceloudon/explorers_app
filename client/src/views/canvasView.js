9116882d0b3c9dafbbd633f982bc0db40166551c
Alice Loudon <alice_loudon@icloud.com>
2017-04-25 15:27:08 +0100

    Change colour of paint tool works

--- client/src/views/canvasView.js
+++ client/src/views/canvasView.js
@@ -8,35 +8,32 @@ CanvasView.prototype = {
   render: function(){
     
     var self = this
+
     this.canvas.onclick = function(event){
       var coords = self.canvas.getBoundingClientRect()
-      console.log(coords.left, coords.top)
-      console.log(event.clientX, event.clientY)
       // self.drawRectangle( (event.clientX - coords.left), (event.clientY - coords.top) )
-      self.drawCircle((event.clientX - coords.left), (event.clientY - coords.top))
+      self.drawCircle( (event.clientX - coords.left), (event.clientY - coords.top) )
     }
 
-    // var tools = document.querySelectorAll('input')
-    // tools.forEach(function(tool){
-    //   tool.onclick = function{
-    //     currentTool = this.value
-    //     canvas.onclick = canvasClickHandler
-    //   }
-    // })
+    var colourPicker = document.querySelector('#change-colour-input')
+    colourPicker.onchange = function(){
+      self.context.fillStyle = this.value
+      self.context.strokeStyle = this.value
+    }
 
   },
 
-  drawRectangle: function(x, y){
-    this.context.fillStyle = 'orange'
-    this.context.fillRect(x, y, 10, 10)
-  },
+  // drawRectangle: function(x, y, strokeColour){
+  //   this.context.strokeStyle = strokeColour
+  //   this.context.fillRect(x, y, 10, 10)
+  // },
 
   drawCircle: function(x, y){
     this.context.beginPath()
     this.context.arc(x, y, 10, 0, Math.PI*2)
-    this.context.fillStyle = 'green'
+    // this.context.fillStyle = 'green'
     this.context.fill()
-    this.context.strokeStyle = 'green'
+    // this.context.strokeStyle = 'green'
     this.context.stroke()
   }
 
