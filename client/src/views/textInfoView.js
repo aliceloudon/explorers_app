714b509acb6ca107b80e098c259f4843bd1c848f
Ross Crichton <rcrichton87@googlemail.com>
2017-04-26 15:27:49 +0100

    info text has a title and font set

--- client/src/views/textInfoView.js
+++ client/src/views/textInfoView.js
@@ -6,9 +6,13 @@ TextInfoView.prototype = {
 
   render: function(explorer){
     this.textInfoView.innerHTML = ''
-    var textBox = document.createElement('div')
+    var title = document.createElement('h3')
+    title.innerText = explorer.title + ' - ' + explorer.date
+    this.textInfoView.appendChild(title)
+    var textBox = document.createElement('p')
     textBox.innerHTML = explorer.info
     this.textInfoView.appendChild(textBox)
+    this.textInfoView.className = 'text-info-view'
   }
 
 }
